c278396fc00aed5671c19b2e4b01693b1f7f6057
Fireblade75 <Fireblade75@users.noreply.github.com>
2018-10-16 23:17:12 +0200

    security upgrade

---
+++ client/constants.js
@@ -0,0 +1,9 @@
+const constants = {
+    ADD_USER = 'ADD_USER',
+    ADD_PLAYER = 'ADD_PLAYER',
+    ADD_MATCH = 'ADD_MATCH',
+    ADD_ERROR = 'ADD_ERROR',
+    CLEAR_ERROR = 'CLEAR_ERROR'
+}
+
+module.exports = constants;
